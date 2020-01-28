dafe23782f27a3f46052025d75a9b16a99178abd
Ian Y.E. Pan <ianpan870102@gmail.com>
2020-01-28 18:23:09 +0800

    Delete ivy-posframe block, use min-width in init.el instead

--- emacs-package-tweaks.el
+++ emacs-package-tweaks.el
@@ -5,15 +5,3 @@
   (:modes python-mode)
   (:format (format-all--buffer-easy executable)))
 
-;; ivy-posframe height/width tweaks
-(defun ivy-posframe-get-size ()
-  "The default functon used by `ivy-posframe-size-function'."
-  (list
-   :height ivy-posframe-height
-   :width ivy-posframe-width
-   :min-height (or ivy-posframe-min-height
-                   (let ((height (+ ivy-height 1)))
-                     (min height (or ivy-posframe-height height))))
-   :min-width (or ivy-posframe-min-width
-                  (let ((width (round (* (frame-width) 0.8))))
-                    (min width (or ivy-posframe-width width))))))
