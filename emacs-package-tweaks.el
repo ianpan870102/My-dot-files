52c41a9254fc8d8406008e66a10f749e9c3f78e2
Ian Y.E. Pan <ianpan870102@gmail.com>
2020-02-23 16:53:53 +0800

    Update emacs-package-tweaks.el

--- emacs-package-tweaks.el
+++ emacs-package-tweaks.el
@@ -1,7 +1,6 @@
-;; format-all yapf
 (define-format-all-formatter yapf
   (:executable "yapf")
   (:install "pip install yapf")
-  (:modes python-mode)
+  (:languages "Python")
   (:format (format-all--buffer-easy executable)))
 
